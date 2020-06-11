4d3fcf0827c17e30b793fa58075fca065e53ba6e
Ashkan Mohseni <amohseni80@yahoo.com>
2020-06-11 11:42:21 -0400

    Updates the sign up scene design

--- src/fx/SignUpController.java
+++ src/fx/SignUpController.java
@@ -1,18 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package fx;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
  *
- * @author ashkanmohseni
+ * @author ashkan mohseni
  */
 public class SignUpController implements Initializable {
 
@@ -23,5 +22,13 @@ public class SignUpController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void create_account(ActionEvent event) {
+    }
+
+    @FXML
+    private void sign_in(MouseEvent event) {
+    }
     
 }
