178e2a0c3b70199164cfa12fd5379224d0991aa8
Ashkan Mohseni <amohseni80@yahoo.com>
2020-06-11 12:01:21 -0400

    Adds the transition between sign up scene and Login scene

--- src/fx/SignUpController.java
+++ src/fx/SignUpController.java
@@ -1,12 +1,18 @@
 
 package fx;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -28,7 +34,14 @@ public class SignUpController implements Initializable {
     }
 
     @FXML
-    private void sign_in(MouseEvent event) {
+    private void sign_in(MouseEvent event) throws IOException {
+        Parent login = FXMLLoader.load(this.getClass().getResource("Login.fxml"));
+        Scene scene = new Scene(login, 1500, 870);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.setMaximized(true);
+        window.show();
     }
     
 }
